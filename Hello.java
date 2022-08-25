6f8b60353fb3eb4e944e5d9fb19a840fa6dce4df
PakJustin <112010926+PakJustin@users.noreply.github.com>
2022-08-24 20:04:44 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*************************************************
+* Hello.java
+**************************************************
+* Justin Pak
+* 2022-08-24
+* Course 255 and Section 004
+**************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Justin Pak");
+		System.out.println("Hello.java");
+		System.out.println("This program will print hello");
+		System.out.println("2022-08-24");
+		System.out.println("Course 255 and Section 004");
+		System.out.println();
+	
+	}
+}
